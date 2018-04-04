38a268cfcbf4fedf88593b136e37e73f6f3b2492
edmondnow <edmond.bitay@gmail.com>
2018-04-04 18:28:05 +0200

    added websockets

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,213 @@
-var currentLabels = []//determine current labels up to a year
-var currentStocks = []; //containers always the last logged stocks
+//stock visualization and stock theme from Highstock
+//https://www.highcharts.com
 
 
-(function getLabels(){
-    $.ajax({
-        type: 'POST',
-        url: "/getlabels",
-        data: {stock: 'MSFT'},
-        success: function(data){
-            currentLabels = data.dataKeys.reverse();
-            makeChart();
-        },
-        error: function(error){
-            console.log(error)
-        }
-        });
-})();
+var series = []
+var names = [];
 
+var socket = io.connect('http://localhost:3000')
 
+Highcharts.createElement('link', {
+   href: 'https://fonts.googleapis.com/css?family=Unica+One',
+   rel: 'stylesheet',
+   type: 'text/css'
+}, null, document.getElementsByTagName('head')[0]);
 
+Highcharts.theme = {
+   colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+      '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+   chart: {
+      backgroundColor: '#2a2a2b',
+      style: {
+         fontFamily: '\'Unica One\', sans-serif'
+      },
+      plotBorderColor: '#606063'
+   },
+   title: {
+      style: {
+         color: '#E0E0E3',
+         textTransform: 'uppercase',
+         fontSize: '20px'
+      }
+   },
+   subtitle: {
+      style: {
+         color: '#E0E0E3',
+         textTransform: 'uppercase'
+      }
+   },
+   xAxis: {
+      gridLineColor: '#707073',
+      labels: {
+         style: {
+            color: '#E0E0E3'
+         }
+      },
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      title: {
+         style: {
+            color: '#A0A0A3'
+
+         }
+      }
+   },
+   yAxis: {
+      gridLineColor: '#707073',
+      labels: {
+         style: {
+            color: '#E0E0E3'
+         }
+      },
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      tickWidth: 1,
+      title: {
+         style: {
+            color: '#A0A0A3'
+         }
+      }
+   },
+   tooltip: {
+      backgroundColor: 'rgba(0, 0, 0, 0.85)',
+      style: {
+         color: '#F0F0F0'
+      }
+   },
+   plotOptions: {
+      series: {
+         dataLabels: {
+            color: '#B0B0B3'
+         },
+         marker: {
+            lineColor: '#333'
+         }
+      },
+      boxplot: {
+         fillColor: '#505053'
+      },
+      candlestick: {
+         lineColor: 'white'
+      },
+      errorbar: {
+         color: 'white'
+      }
+   },
+   legend: {
+      itemStyle: {
+         color: '#E0E0E3'
+      },
+      itemHoverStyle: {
+         color: '#FFF'
+      },
+      itemHiddenStyle: {
+         color: '#606063'
+      }
+   },
+   credits: {
+      style: {
+         color: '#666'
+      }
+   },
+   labels: {
+      style: {
+         color: '#707073'
+      }
+   },
+
+   drilldown: {
+      activeAxisLabelStyle: {
+         color: '#F0F0F3'
+      },
+      activeDataLabelStyle: {
+         color: '#F0F0F3'
+      }
+   },
+
+   navigation: {
+      buttonOptions: {
+         symbolStroke: '#DDDDDD',
+         theme: {
+            fill: '#505053'
+         }
+      }
+   },
+
+   // scroll charts
+   rangeSelector: {
+      buttonTheme: {
+         fill: '#505053',
+         stroke: '#000000',
+         style: {
+            color: '#CCC'
+         },
+         states: {
+            hover: {
+               fill: '#707073',
+               stroke: '#000000',
+               style: {
+                  color: 'white'
+               }
+            },
+            select: {
+               fill: '#000003',
+               stroke: '#000000',
+               style: {
+                  color: 'white'
+               }
+            }
+         }
+      },
+      inputBoxBorderColor: '#505053',
+      inputStyle: {
+         backgroundColor: '#333',
+         color: 'silver'
+      },
+      labelStyle: {
+         color: 'silver'
+      }
+   },
+
+   navigator: {
+      handles: {
+         backgroundColor: '#666',
+         borderColor: '#AAA'
+      },
+      outlineColor: '#CCC',
+      maskFill: 'rgba(255,255,255,0.1)',
+      series: {
+         color: '#7798BF',
+         lineColor: '#A6C7ED'
+      },
+      xAxis: {
+         gridLineColor: '#505053'
+      }
+   },
+
+   scrollbar: {
+      barBackgroundColor: '#808083',
+      barBorderColor: '#808083',
+      buttonArrowColor: '#CCC',
+      buttonBackgroundColor: '#606063',
+      buttonBorderColor: '#606063',
+      rifleColor: '#FFF',
+      trackBackgroundColor: '#404043',
+      trackBorderColor: '#404043'
+   },
+
+   // special colors for some of the
+   legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+   background2: '#505053',
+   dataLabelsColor: '#B0B0B3',
+   textColor: '#C0C0C0',
+   contrastTextColor: '#F0F0F3',
+   maskColor: 'rgba(255,255,255,0.3)'
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
 
 
 
@@ -27,78 +216,106 @@ $('#submit').click(function(){
     $.ajax({
         type: 'POST',
         url: "/getstock",
-        data: {stock: stock, labels: currentLabels},
-        success: function(data){
-        currentStocks.push({
-            label: data.name,
-            backgroundColor: 'yellow',
-            borderColor: 'yellow',
-            data: data.dataValues,
-            borderWidth: 1
-
-        });
-         makeChart();
-         addCont(data.name)
+        data: {stock: stock},
+        success: function(data){ 
+            $('.error').empty()     
+            series.push({
+                name: data.name,
+                data: data.dataArrays,
+            });
+            addCont(data.name);
+            names.push(data.name);
+            socket.emit('stock', {
+                series: series,
+                name: data.name
+            });
+            createChart();
+             
         },
         error: function(error){
-            console.log(error)
+            if($('.error').text().length==0){
+              $('.error').append('Stock does not exist.')  
+            }
+            
         }
         });
 });
 
+socket.on('stock', function(data){
+    series = data.series;
+    addCont(data.name);
+    createChart();
+});
 
+socket.on('remove', function(data){
+    series = data.series;
+    $("#" + data.name).remove();
+    createChart();
+});
 
+function createChart() {
 
+    Highcharts.stockChart('container', {
+
+        rangeSelector: {
+            selected: 0
+        },
+
+        yAxis: {
+            labels: {
+                formatter: function () {
+                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                }
+            },
+            plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'silver'
+            }]
+        },
+
+        plotOptions: {
+            series: {
+                compare: 'percent',
+                showInNavigator: true
+            }
+        },
+
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            valueDecimals: 2,
+            split: true
+        },
+
+        series: series
+    });
+}
+
+$('.stocks-container').delegate('i','click',  function(){
+    var stock = this.closest('div').id.toString();
+    names.splice(names.indexOf(stock,1));
+    for(var i = 0; i<series.length; i++){
+        if(series[i].name.toUpperCase() == stock.toUpperCase() ){
+            series.splice(i, 1);
+            createChart();
+        }
+    }
+    socket.emit('remove', {
+        series: series,
+        name: stock
+    });
+
+    this.closest('div').remove();
+});
 
 
 function addCont(name){
-    var stockContainer = '<div class="stock-container">'
+    var stockContainer = '<div class="stock-container" id="'+ name + '">'
     stockContainer += '<i class="fa fa-times"></i>'
     stockContainer += '<h1 class="name">' + name + '</h1>'
     stockContainer += '<p class="desc">' + 'desciprtion var' + '</p>'
     stockContainer += '</div>'
-    $('.stocks-container').prepend(stockContainer)
+    $('.stocks-container').append(stockContainer)
 }
 
-
-
-
-
-
-
-function makeChart(labels, stocks){
-
-    var ctx = $('#stocks');
-
-    var options = {
-      maintainAspectRatio: false,
-      scales: {
-        yAxes: [{
-          stacked: true,
-          gridLines: {
-            display: true,
-            color: "rgba(255,99,132,0.2)"
-          }
-        }],
-        xAxes: [{
-          gridLines: {
-            display: false
-          }
-        }]
-      }
-    }
-
-    var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels:currentLabels,
-        datasets: currentStocks
-    },
-
-    options: options
-    });
-
-
-}
-
-
+createChart();
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const keys = require('../config/config');
-const alpha = require('alphavantage')({ key: keys.alphaKey });
+const request = require('request');
+const moment = require('moment');
 
-
-/* GET home page. */
 
 router.get('/', function(req, res, next){
   res.render('index');
@@ -12,17 +11,23 @@ router.get('/', function(req, res, next){
 
 router.post('/getstock', function(req, res, next) {
   var stock = req.body.stock;
-  var keys = req.body['labels[]'];
-  console.log(stock);
-  alpha.data.daily(stock, 'full').then(data => {
-    data = data['Time Series (Daily)'];
-    var dataValues = []
-    for(i = 0; i<keys.length; i++){
-      var dataPoint = data[keys[i]]['1. open'];
-      dataValues.push(dataPoint)
+  var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&apikey='+ keys.alphaKey + '&symbol=' + stock;
+  request(url, function (err, response, body) {
+    if(body.indexOf('Error Message')>-1){
+      res.status(500).send('No such stock.')
+    } else {
+    body = JSON.parse(body);
+    var data = body['Time Series (Daily)'];
+    var dataArrays = [];
+    var dataKeys = Object.keys(data).slice(0, 700);
+    for(i = 0; i<dataKeys.length; i++){
+      var dataPoint = data[dataKeys[i]]['1. open'];
+      var date = moment(dataKeys[i]).valueOf()
+      dataArrays.push([date, parseFloat(dataPoint)])
     }
-    console.log(dataValues.length)
-    res.send({dataValues: dataValues, name: req.body.stock});
+    dataArrays = dataArrays.reverse();
+    res.send({dataArrays: dataArrays, name: req.body.stock});
+    }
   });
 });
 
@@ -36,37 +41,5 @@ router.post('/getlabels', function(req, res, next) {
   res.send({dataKeys: dataKeys});
   });
 });
-
-
-/*
-
-alpha.data.intraday(symbol, outputsize, datatype, interval)
-alpha.data.daily(symbol, outputsize, datatype, interval)
-alpha.data.daily_adjusted(symbol, outputsize, datatype, interval)
-alpha.data.weekly(symbol, outputsize, datatype, interval)
-alpha.data.weekly_adjusted(symbol, outputsize, datatype, interval)
-alpha.data.monthly(symbol, outputsize, datatype, interval)
-alpha.data.monthly_adjusted(symbol, outputsize, datatype, interval)
-alpha.data.batch([symbol1, symbol2..])
-
-
-Symbol:
-The name of the equity of your choice. For example: symbol=MSFT
-
-Interval: 
-Time interval between two consecutive data points in the time series. The following values are supported: 1min, 5min, 15min, 30min, 
-
-Optional: outputsize
-
-By default, outputsize=compact. Strings compact and full are accepted with the following specifications: compact returns only the latest 100 data points in the intraday time series;
- full returns the full-length intraday time series. The "compact" option is recommended if you would like to reduce the data size of each API call.
-
-❚ Optional: datatype
-
-By default, datatype=json
-
-
-*/
-
 
 module.exports = router;
